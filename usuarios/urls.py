67467dc9c392d9e07a07394ceaaf32d738d35522
Matheus Oliveira <matheuseu41@gmail.com>
2024-09-14 16:02:51 -0300

    Implementando lógica e template de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('cadastrar_vendedor/', views.cadastrar_vendedor, name="cadastrar_vendedor")
+    path('cadastrar_vendedor/', views.cadastrar_vendedor, name="cadastrar_vendedor"),
+    path('login/', views.login, name="login")
 ]
