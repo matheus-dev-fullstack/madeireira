4aef4576cf0442011f4d3d713b94fd1d8c8e486f
Matheus Oliveira <matheuseu41@gmail.com>
2024-09-14 12:52:59 -0300

    Configuração da View e Template para criação de vendedor

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rolepermissions.decorators import has_permission_decorator
+from .models import Users
 
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
-    return HttpResponse('Teste')
+    if request.method == 'GET':
+        return render(request, 'cadastrar_vendedor.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        user = Users.objects.filter(email=email)
+        
+        if user.exists():
+            # TODO: utilizar messages do django
+            return HttpResponse('Já existe um vendedor com este email')
+        
+        user = Users.objects.create_user(username=email, email=email, password=senha)
+        
+        # TODO: Redirecionar com uma mensagem
+        return HttpResponse('Vendedor criado com sucesso')
