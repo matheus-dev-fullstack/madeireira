57b19a4aad71c041fd21e3fde828667a215decd5
Matheus Oliveira <matheuseu41@gmail.com>
2024-09-14 12:08:54 -0300

    Criação do Template base

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rolepermissions.decorators import has_permission_decorator
 
+@has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
     return HttpResponse('Teste')
