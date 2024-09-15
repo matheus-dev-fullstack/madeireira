ff5da7e69270aa5177dde80acf9c35995959bf4c
Matheus Oliveira <matheuseu41@gmail.com>
2024-09-15 10:43:59 -0300

    Configurando Settings.py para lidar com os arquivos estáticos

--- madeireira/settings.py
+++ madeireira/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR + 'templates/static'), )
+STATIC_ROOT = os.path.join('static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
