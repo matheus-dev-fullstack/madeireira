d70fb5ebeaa84c946469018286127aaad480eec0
Matheus Oliveira <matheuseu41@gmail.com>
2024-09-19 07:18:02 -0300

    Adicionando class para imagens dos produtos no model

--- estoque/models.py
+++ estoque/models.py
@@ -23,3 +23,7 @@ class Produto(models.Model):
         lucro = self.preco_venda - self.preco_compra
         return (lucro * 100) / self.preco_compra
         # Vai retornar em porcentagem
+        
+class Imagem(models.Model):
+    imagem = models.ImageField(upload_to='imagem_produto')
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
